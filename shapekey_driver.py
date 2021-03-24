867d1146770ac6da354cd760d80c35f2e8150158
obaDesktop <neverknowsbest315@gmail.com>
2021-03-24 22:07:59 +0900

    UIを改善、使用されないプロパティを削除する仕様に、探索範囲をアーマチュアを親に持つオブジェクトのみに変更

--- shapekey_driver.py
+++ shapekey_driver.py
@@ -1,5 +1,6 @@
 import bpy
 from bpy.props import (BoolProperty, IntProperty, FloatProperty, PointerProperty)
+import re
 
 
 bl_info = {
@@ -40,11 +41,11 @@ def add_driver(
     else:
         v = d.variables[0]
             
-    print (target)
+    #print (target)
     v.name                 = prop
     v.targets[0].id        = target
     v.targets[0].data_path = dataPath
-    print ("ppp")
+    #print ("ppp")
     d.expression = func + "(" + v.name + ")" if func else v.name
     d.expression = d.expression if not negative else "-1 * " + d.expression
 
@@ -53,39 +54,85 @@ def add_driver(
 class SearchShapeKeys(bpy.types.Operator):
     bl_idname = "object.searchshapekeys_or"
     bl_label = "NOP"
-    bl_description = "何もしない"
+    bl_description = "指定したアーマチュアを親に持つオブジェクトのシェイプキーにドライバを設定します。"
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
         
+        scene = context.scene
         
-        '''ボーンを設定します'''
-        arm = bpy.data.objects['_arm_nizi']
-        bone_name = 'facial'
         
-        ''''''''''''''''''''''''
+        ########### ボーンを設定します ##############
+        
+        arm = scene.shapekey_amt
+        bone_name = scene.shapekey_bone
+        
+        if(arm.type != 'ARMATURE' or bone_name == '') :
+            raise Exception("アーマチュア、ボーンを設定して下さい")
+            return  {'CANCELLED'}
+            
+        #############################################
         
         
         KeySetList = bpy.data.shape_keys
         
-        for keyset in KeySetList:
-            print("\n",keyset.user)
-            for key in keyset.key_blocks:
-                print(key.name)
+        
+        bone = arm.pose.bones[bone_name]
+        
+        shape_list = []
+        
+        for obj in scene.objects:
+            if obj.type == 'MESH' and obj.data != None and obj.data.shape_keys != None:
                 
-                if key.name == "Basis" or key.name == "ベース":
-                    continue
-                
-                #プロパティの追加
-                if key.name not in arm.pose.bones[bone_name]:
-                    arm.pose.bones[bone_name][key.name] = 0.0
+                keyset = obj.data.shape_keys
                     
-                #ドライバの設定
-                add_driver( 
-                    key, arm, 'value',
-                    'pose.bones["' + bone_name + '"]["' + key.name + '"]'
-                    )
-               
+                # 指定したアーマチュアを親に持つ?
+                p     = obj
+                flag  = False    
+                while p != None :
+                    p = p.parent
+                    if p == arm :
+                        flag = True
+                        break
+                
+                if flag :
+                    
+                    print("\nUSER : ",keyset.user.name)
+                    for key in keyset.key_blocks:
+                        
+                        print("    ", key.name)
+                        
+                        if key.name == "Basis" or key.name == "ベース":
+                            continue
+                        
+                        # 記憶
+                        if not key.name in shape_list :
+                            shape_list.append(key.name)
+                        
+                        #プロパティの追加
+                        if key.name not in arm.pose.bones[bone_name]:
+                            bone[key.name] = 0.0
+                            
+                        #ドライバの設定
+                        add_driver( 
+                            key, arm, 'value',
+                            'pose.bones["' + bone_name + '"]["' + key.name + '"]'
+                            )
+                    
+        # 削除されたシェイプキーのプロパティを削除
+        shape_list.sort()
+        shape_list_prev = re.split('\', \'|\'\]|\[\'', scene.shapekey_list)[1:-1]    # 処理前のシェイプキーの一覧
+        
+        for sk in shape_list_prev :
+            if not sk in shape_list :
+                print("delete : ", sk)
+                del bone[sk]
+        
+        scene.shapekey_list = str(shape_list)
+        
+        print (scene.shapekey_list )
+             
+        
         return {'FINISHED'}
 
     
@@ -114,10 +161,32 @@ class MYADDON_PT_SetShapeofMouthUI(bpy.types.Panel):
     def draw(self, context):
        
         layout = self.layout
-       
+        scene = context.scene
         
-        layout.label(text="シェイプキーを検索してボーンfacialのプロパティをドライバとして設定します")
-        layout.operator(SearchShapeKeys.bl_idname, text="update")
+        # input Armature
+        layout.prop_search(scene, "shapekey_amt", context.scene,  "objects", text = "Armature")
+        if scene.shapekey_amt and scene.shapekey_amt.type == 'ARMATURE':
+        
+            # input bone
+            layout.prop_search(scene, "shapekey_bone", scene.shapekey_amt.data, "bones", text="Bone")    
+            if scene.shapekey_bone != '' :
+        
+                layout.separator()
+        
+                layout.label(text="シェイプキーを検索してボーンfacialのプロパティをドライバとして設定します")
+                layout.operator(SearchShapeKeys.bl_idname, text="refresh")
+                
+                layout.separator()
+                
+                # プロパティを表示
+                shape_list = re.split('\', \'|\'\]|\[\'', scene.shapekey_list)[1:-1]  # この処理無駄そう。。。
+                for n in shape_list :
+                    row = layout.row(align=True)
+                    box = row.box()
+                    split = box.split(factor=1.0)
+                    row = split.row(align=True)
+                    row.label(text = n)
+                    row.prop(scene.shapekey_amt.pose.bones[scene.shapekey_bone], '["%s"]' %n, text="")
         
        
 classes = [
@@ -128,8 +197,15 @@ classes = [
 def register():
     for c in classes:
         bpy.utils.register_class(c)
+    
+    # シーンのカスタムプロパティとして定義
+    # オブジェクトは適切なプロパティがないので、ポインタープロパティとして型を指定
+    bpy.types.Scene.shapekey_amt = bpy.props.PointerProperty(type=bpy.types.Object)
+    # BoneはなぜかStringProperty
+    bpy.types.Scene.shapekey_bone = bpy.props.StringProperty()
+    bpy.types.Scene.shapekey_list = bpy.props.StringProperty()
+            
     print("Addon 'shapekey_driver' is Active")
-    #bpy.types.Object.sample_props=PointerProperty(type=SampleProperties) 
         
 
 def unregister():
